58c014230f75668d781373588996ca11102bcb5a
Don Morrison <don@elskwid.net>
2014-01-08 00:21:39 -0800

    Update Spec to use new modules

--- lib/rack-minitest/spec.rb
+++ lib/rack-minitest/spec.rb
@@ -1,6 +1,8 @@
+require "rack-minitest/expectations"
 require "rack-minitest/json"
 
 class Minitest::Spec
   include Rack::Test::Methods
+  include Rack::Minitest::Expectations
   include Rack::Minitest::JSON
 end
