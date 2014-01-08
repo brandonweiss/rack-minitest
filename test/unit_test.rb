92437cfb1ce8b9fcfb692f45120d7df171752ecb
Don Morrison <don@elskwid.net>
2014-01-08 00:50:35 -0800

    Add Unit test support
    Add Rack::Minitest::Unit which mixes in the rack 
    test methods, the expectations (since in Minitest 
    you can mix and match unit and spec style), and 
    the JSON methods. Add a unit style test to ensure
    the mixing are wired up correctly.

---
+++ test/unit_test.rb
@@ -0,0 +1,39 @@
+require File.expand_path("../test_helper", __FILE__)
+
+class TestRackMinitestUnit < Minitest::Test
+
+  def app
+    json = { "foo" => "bar" }.to_json
+    lambda { |env| [200, { "Content-Type" => "text/html" }, [json]] }
+  end
+
+  def test_includes_rack_test_methods
+    assert Minitest::Test.include? Rack::Test::Methods
+  end
+
+  def test_parses_JSON_responses
+    get "/"
+    assert_equal({ "foo" => "bar" }, last_json_response)
+  end
+
+  def test_get_as_JSON
+    get_json "/"
+    assert_equal "application/json", last_request.env["CONTENT_TYPE"]
+  end
+
+  def test_post_as_JSON
+    post_json "/"
+    assert_equal "application/json", last_request.env["CONTENT_TYPE"]
+  end
+
+  def test_put_as_JSON
+    put_json "/"
+    assert_equal "application/json", last_request.env["CONTENT_TYPE"]
+  end
+
+  def test_delete_as_JSON
+    delete_json "/"
+    assert_equal "application/json", last_request.env["CONTENT_TYPE"]
+  end
+
+end
