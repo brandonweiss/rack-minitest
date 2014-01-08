1175ba068f626822694c6bd5fcaba6106b9707fd
Don Morrison <don@elskwid.net>
2014-01-08 00:12:44 -0800

    Separate assertions and expectations
    Move expectations to a file that requires and includes the
    assertions. This will allow us to add either the assertions or
    the expectations to support Minitest unit tests. Also put
    these inside the Rack::Minitest module.

--- lib/rack-minitest/assertions.rb
+++ lib/rack-minitest/assertions.rb
@@ -1,4 +1,4 @@
-module Minitest::Assertions
+module Rack::Minitest::Assertions
 
   def assert_ok(response)
     assert_response_status response, 200
@@ -45,14 +45,3 @@ module Minitest::Assertions
   end
 
 end
-
-Rack::MockResponse.infect_an_assertion :assert_ok,                    :must_be_ok,                    :only_one_argument
-Rack::MockResponse.infect_an_assertion :assert_created,               :must_be_created,               :only_one_argument
-Rack::MockResponse.infect_an_assertion :assert_no_content,            :must_be_no_content,            :only_one_argument
-Rack::MockResponse.infect_an_assertion :assert_moved_permanently,     :must_be_moved_permanently,     :only_one_argument
-Rack::MockResponse.infect_an_assertion :assert_bad_request,           :must_be_bad_request,           :only_one_argument
-Rack::MockResponse.infect_an_assertion :assert_unauthorized,          :must_be_unauthorized,          :only_one_argument
-Rack::MockResponse.infect_an_assertion :assert_forbidden,             :must_be_forbidden,             :only_one_argument
-Rack::MockResponse.infect_an_assertion :assert_not_found,             :must_be_not_found,             :only_one_argument
-Rack::MockResponse.infect_an_assertion :assert_unprocessable_entity,  :must_be_unprocessable_entity,  :only_one_argument
-Rack::MockResponse.infect_an_assertion :assert_internal_server_error, :must_be_internal_server_error, :only_one_argument
---
+++ lib/rack-minitest/expectations.rb
@@ -0,0 +1,18 @@
+require "rack-minitest/assertions"
+
+module Rack::Minitest::Expectations
+  include Rack::Minitest::Assertions
+end
+
+class Rack::MockResponse
+  infect_an_assertion :assert_ok,                    :must_be_ok,                    :only_one_argument
+  infect_an_assertion :assert_created,               :must_be_created,               :only_one_argument
+  infect_an_assertion :assert_no_content,            :must_be_no_content,            :only_one_argument
+  infect_an_assertion :assert_moved_permanently,     :must_be_moved_permanently,     :only_one_argument
+  infect_an_assertion :assert_bad_request,           :must_be_bad_request,           :only_one_argument
+  infect_an_assertion :assert_unauthorized,          :must_be_unauthorized,          :only_one_argument
+  infect_an_assertion :assert_forbidden,             :must_be_forbidden,             :only_one_argument
+  infect_an_assertion :assert_not_found,             :must_be_not_found,             :only_one_argument
+  infect_an_assertion :assert_unprocessable_entity,  :must_be_unprocessable_entity,  :only_one_argument
+  infect_an_assertion :assert_internal_server_error, :must_be_internal_server_error, :only_one_argument
+end
